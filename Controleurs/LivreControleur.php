5d07cc85fa643910cea6cf105fc913ad2e39c611
Sanddjam <djama.ams@gmail.com>
2020-11-25 22:41:03 +0100

    Retrait var_dump LivreControleurs

--- Controleurs/LivreControleur.php
+++ Controleurs/LivreControleur.php
@@ -7,13 +7,15 @@ Contrairement à un site développé en procédural, lorsque l'on veut ajouter u
 class LivreControleur{
     public function liste(){
 
+        echo "<h1>Liste des livres</h1>";
+        
         include "Modeles/Bdd.php";
         $livres = Bdd::listeLivre();
         
         include "vues/livre/table.html.php";
 
-        echo "<h1>Liste des livres</h1>";
-        echo "<pre>"; var_dump($livres); echo "</pre>";
+        
+        
     }
 
     public function ajouter(){
