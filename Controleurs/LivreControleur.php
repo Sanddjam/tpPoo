d1255c4912c14499c8858aa1c19656a5fe37df59
Sanddjam <djama.ams@gmail.com>
2020-11-26 12:57:20 +0100

    rien

--- Controleurs/LivreControleur.php
+++ Controleurs/LivreControleur.php
@@ -36,7 +36,7 @@ class LivreControleur{
  * après extract() il exite 2 variables: $nom ='Cérien' et $prenom="Jean"
  */
     extract($parametresVue);
-    
+
 /**ob_start() permet de mettre l'affichage en 'pause'. Le html qui est généré est mis dans une mémoire tampon. Il ne sera pas affiché */
 ob_start();
 include "vues/$fichierVue";
