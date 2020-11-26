383b11dd50b40dd345594e9d577da7d27daa332e
Sanddjam <djama.ams@gmail.com>
2020-11-26 15:27:08 +0100

    ajout conditions et

--- index.php
+++ index.php
@@ -8,23 +8,23 @@ $pdo = new PDO("mysql:host=localhost;dbname=biblio;charset=utf8", "root", "");
  * Contrôleur : cette partie va faire le lien entre les 2 autres parties, c'est aussi ici que seront fait tous les traitements des données, las algorithmes... 
  * 
  * Dans un site développé selon le MVC, un seul fichier va servir de point d'entrée :
-*index.php (c'est-à-dire que c'est le seul fichier qui sera appelé par les navigateurs
-   * des internautes). 
-    *Ce fichier servira aussi de routeur : il détermine quel Contrôleur doit être appelé
+ *index.php (c'est-à-dire que c'est le seul fichier qui sera appelé par les navigateurs
+ * des internautes). 
+ *Ce fichier servira aussi de routeur : il détermine quel Contrôleur doit être appelé
  */
 
- // ex: /index.php?controleur=livre&methode=liste
+// ex: /index.php?controleur=livre&methode=liste
 
- $controleur = $_GET["controleur"];
- $methode = $_GET["methode"];
-
- /*ucfirst : met une majuscule au début d'une chaine de caractère */
+$controleur = !empty($_GET["controleur"]) ? $_GET["controleur"] : "accueil";
+$methode = !empty($_GET["methode"]) ? $_GET["methode"] : "index";
+$id = !empty($_GET["id"]) ? $_GET["id"] : null;
+/*ucfirst : met une majuscule au début d'une chaine de caractère */
 $controleur = ucfirst($controleur); //ex: Livre
 //$classeControleur = $controleur . "Controleur"; //ex: LivreControleur
 $classeControleur = "Controleurs\\" . $controleur . "Controleur"; //ex: LivreControleur
 
 
- //Création d'un objet LivreControleur
+//Création d'un objet LivreControleur
 // Au lieu de taper explicitement le nom de la classe que je veux instancier, j'utillise une variable
- $ctrl = new $classeControleur;
- $ctrl->$methode();
+$ctrl = new $classeControleur;
+$ctrl->$methode($id);
