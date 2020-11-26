0168d48699a4ff899d734ef6f1615cba78508092
Sanddjam <djama.ams@gmail.com>
2020-11-26 15:26:00 +0100

    Ajout fonction ajouter et modifier

--- Controleurs/LivreControleur.php
+++ Controleurs/LivreControleur.php
@@ -10,57 +10,84 @@ Contrairement à un site développé en procédural, lorsque l'on veut ajouter u
  * ex: LivreControleur devient Controleurs\LivreControleur
  * Le namespace va copier l'arborescence des dossiers : par exemple les classes Controleur sont dans le dossier Controeleurs donc dans le namespace Controleurs.
  */
-namespace Controleurs;
- use Modeles\Bdd; // indique que dans ce fichier je vais utiliser cette classe et du coup plus besoin d'utiliser le chemin de la ligne 23.
- use Modeles\Livre;
 
-class LivreControleur{
-    public function liste(){
+namespace Controleurs;
+
+use Modeles\Bdd; // indique que dans ce fichier je vais utiliser cette classe et du coup plus besoin d'utiliser le chemin de la ligne 23.
+use Modeles\Livre;
+
+class LivreControleur
+{
+    public function liste()
+    {
 
         echo "<h1>Liste des livres</h1>";
-        
+
         //include "Modeles/Bdd.php";
         //$livres = Bdd::listeLivre();
         //$livres = \Modeles\Bdd::liste("livre");
         $livres = Bdd::liste("livre");
         $this->rendu("livre/table.html.php", ["livres" => $livres]);
-        
     }
 
-    public function ajouter(){
-        if($_POST){
+    public function ajouter()
+    {
+        if ($_POST) {
             extract($_POST);
-            if(!empty($titre) && !empty($auteur)){
+            if (!empty($titre) && !empty($auteur)) {
                 $livre = new Livre;
                 $livre->setTitre($titre);
                 $livre->setAuteur($auteur);
                 $resultat = Bdd::enregistrerLivre($livre);
-                if($resultat){
+                if ($resultat) {
                     header("Location:index.php?controleur=livre&methode=liste");
                     exit;
                 }
             }
         }
-       $this->rendu("livre/formulaire.html.php");
+        $this->rendu("livre/formulaire.html.php");
     }
 
-    public function rendu($fichierVue, $parametresVue=[]){
-/** extract() va créer autant de variables qu'il y a d'indices dans un array associatif
- * ex: [ "nom" => "Cérien", "prenom" => "Jean"]
- * après extract() il exite 2 variables: $nom ='Cérien' et $prenom="Jean"
- */
-    extract($parametresVue);
+    public function modifier($id)
+    {
+        if (is_numeric($id)) {
+            $livre = Bdd::selectionner("livre", $id);
+            if ($livre) {
+                if ($_POST) {
+                    extract($_POST);
+                    if (!empty($titre) && !empty($auteur)) {
+                        
+                        $livre->setTitre($titre);
+                        $livre->setAuteur($auteur);
+                        $resultat = Bdd::enregistrerLivre($livre);
+                        if ($resultat) {
+                            header("Location:index.php?controleur=livre&methode=liste");
+                            exit;
+                        }
+                    }
+                }
+                $this->rendu("livre/formulaire.html.php");
+            }
+        }
+    }
 
-/**ob_start() permet de mettre l'affichage en 'pause'. Le html qui est généré est mis dans une mémoire tampon. Il ne sera pas affiché */
-ob_start();
-include "vues/$fichierVue";
+    public function rendu($fichierVue, $parametresVue = [])
+    {
+        /** extract() va créer autant de variables qu'il y a d'indices dans un array associatif
+         * ex: [ "nom" => "Cérien", "prenom" => "Jean"]
+         * après extract() il exite 2 variables: $nom ='Cérien' et $prenom="Jean"
+         */
+        extract($parametresVue);
 
-/** On va mettre ces qui est en mémoire tampon dans une variable */
+        /**ob_start() permet de mettre l'affichage en 'pause'. Le html qui est généré est mis dans une mémoire tampon. Il ne sera pas affiché */
+        ob_start();
+        include "vues/$fichierVue";
 
-$contenu = ob_get_contents();
-/** on arrête de mettre l'affichage en mémoire tampon */
-ob_end_clean();
-include "vues/base.html.php";
+        /** On va mettre ces qui est en mémoire tampon dans une variable */
 
+        $contenu = ob_get_contents();
+        /** on arrête de mettre l'affichage en mémoire tampon */
+        ob_end_clean();
+        include "vues/base.html.php";
     }
 }
