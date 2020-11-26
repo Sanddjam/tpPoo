8ba4c1568c775a5686c8b39aebfa3a382002ef37
Sanddjam <djama.ams@gmail.com>
2020-11-26 13:11:27 +0100

    Ajout des notions namespace et use, Modification de la fonction liste()

--- Modeles/Bdd.php
+++ Modeles/Bdd.php
@@ -1,6 +1,16 @@
 <?php
 
-class Bdd{
+namespace Modeles;
+
+/*indique qu'on veut utiliser la classe PDO. on indique les classes que l'on va utiliser dans ce fichier PHP qui ne sont pas dans le même namespace. 
+cela permet de créer un alias de la classe*/
+use PDO;
+
+/**
+ * Un classe abstraite ne peut pas être instanciée (on ne peut pas faire new Bdd).
+ * Cette classe servira à exécuter des méthodes statiques ou pourra être héritée.
+ */
+abstract class Bdd{
 
     /**
      * Une méthode statique (ou méthode de classe) n'est pas liée à un objet mais à une classe.
@@ -16,7 +26,7 @@ class Bdd{
         // PDO::FETCH_CLASS : pour récupérer les enregistrements d'une table sous forme d'objets plutôt que sous forme d'array
 
         // Le 2e argument est le nom de la classe à utiliser
-        $livres = $pdostatement->fetchAll(PDO::FETCH_CLASS, "Livre");
+        $livres = $pdostatement->fetchAll(\PDO::FETCH_CLASS, "Livre");
         return $livres;
 
     } 
@@ -26,7 +36,7 @@ class Bdd{
         $pdostatement = $pdo->query("SELECT * FROM abonne");
         require "Modeles/Abonne.php";
 
-        $abonnes = $pdostatement->fetchAll(PDO::FETCH_CLASS, "Abonne");
+        $abonnes = $pdostatement->fetchAll(\PDO::FETCH_CLASS, "Modeles\Abonne");
 
         return $abonnes;
     }
@@ -41,9 +51,10 @@ class Bdd{
         $pdostatement = $pdo->query("SELECT * FROM $table");
         if( $pdostatement ){
             // temporaire
-            require "Modeles/" . ucfirst($table) . ".php";
+            //require "Modeles/" . ucfirst($table) . ".php";
             //
-            return $pdostatement->fetchAll(PDO::FETCH_CLASS, ucfirst($table));
+            $classe = "Modeles\\" . ucfirst($table);
+            return $pdostatement->fetchAll(PDO::FETCH_CLASS, $classe);
         }
     }
 }
