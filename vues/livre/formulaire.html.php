5548a77db9d1287d02558bc7e8c1edfc4f32bdb7
Sanddjam <djama.ams@gmail.com>
2020-11-26 13:47:40 +0100

    Création du formulaire livre

---
+++ vues/livre/formulaire.html.php
@@ -0,0 +1,14 @@
+<form action="" method="post">
+    <div class="form-group">
+        <label for="titre">Titre</label>
+        <input type="text" id="titre" name="titre" class="form-control">
+    </div>
+    <div>
+        <label for="auteur">Auteur</label>
+        <input type="text" id="auteur" name="auteur" class="form-control">
+    
+</div>
+
+<button class="btn btn-primary">Enregistrer</button>
+<a href="?controleur=livre&methode=liste">Annuler</a>
+</form>
