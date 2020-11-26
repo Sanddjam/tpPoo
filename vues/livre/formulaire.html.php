524f735db2506be5bc8c9387484a2852353b50c2
Sanddjam <djama.ams@gmail.com>
2020-11-26 15:48:41 +0100

    Ajoute de l'attribut value dans input

--- vues/livre/formulaire.html.php
+++ vues/livre/formulaire.html.php
@@ -1,14 +1,14 @@
 <form action="" method="post">
     <div class="form-group">
         <label for="titre">Titre</label>
-        <input type="text" id="titre" name="titre" class="form-control">
+        <input type="text" id="titre" name="titre" class="form-control" required value="<?= !empty($livre) ? $livre->getTitre() : ""?> ">
     </div>
     <div>
         <label for="auteur">Auteur</label>
-        <input type="text" id="auteur" name="auteur" class="form-control">
+        <input type="text" id="auteur" name="auteur" class="form-control" required value="<?= !empty($livre) ? $livre->getAuteur() : ""?>" >
     
 </div>
 
 <button class="btn btn-primary">Enregistrer</button>
-<a href="?controleur=livre&methode=liste">Annuler</a>
+<a href="?controleur=livre&methode=liste" class="btn btn-danger">Annuler</a>
 </form>
